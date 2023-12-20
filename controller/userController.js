a51708d03c5f3c926492047da66077a976e85c08
sleeping-slopes <weareinwin.rar@gmail.com>
2023-12-21 02:39:00 +0300

    lint + auth check + follow/unfollow

--- controller/userController.js
+++ controller/userController.js
@@ -5,23 +5,13 @@ const connection = require('../settings/database')
 
 exports.getByVerifiedJWT = (req,res) =>
 {
-    if (!req.user) response.status(404,'No auth',res);
-    else
+    if (!req.user) return response.status(404,'No auth',res);
     connection.query('SELECT `login` FROM `users` WHERE `login` = ?',[req.user.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else if (rows.length<1)
-        {
-            response.status(404,'User not found',res);
-        }
-        else
-        {
-            const row = rows[0];
-            response.status(200,row,res);
-        }
+        if (error) return response.status(400,error,res);
+        if (rows.length<1) return response.status(404,'User not found',res);
+        const row = rows[0];
+        return response.status(200,row,res);
     })
 }
 
@@ -29,19 +19,10 @@ exports.getUsername = (req,res) =>
 {
     connection.query('SELECT `username` FROM `users` WHERE `login` = ?',[req.params.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else if (rows.length<1)
-        {
-            response.status(404,'User not found',res);
-        }
-        else
-        {
-            const row = rows[0];
-            response.status(200,row,res);
-        }
+        if (error) return response.status(400,error,res);
+        if (rows.length<1) return response.status(404,'User not found',res);
+        const row = rows[0];
+        return response.status(200,row,res);
     })
 }
 
@@ -49,40 +30,26 @@ exports.getProfile = (req,res) =>
 {
     connection.query('SELECT * FROM `view_user_profile` WHERE `login` = ?',[req.params.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else if (rows.length<1)
-        {
-            response.status(404,'User not found',res);
-        }
-        else
-        {
-            const row = rows[0];
-            if (req.user && req.user.login==req.params.login) row.owner=true;
-            response.status(200,row,res);
-        }
-    })
-}
+        if (error) return response.status(400,error,res);
+        if (rows.length<1) return response.status(404,'User not found',res);
 
-exports.getShortProfile = (req,res) =>
-{
-    connection.query('SELECT `username`,`followers_count` FROM `view_user_profile` WHERE `login` = ?',[req.params.login],(error,rows,fields)=>
-    {
-        if (error)
+        const row = rows[0];
+        if (req.user)
         {
-            response.status(400,error,res);
+            if (req.user.login==req.params.login) row.me=true;
+            connection.query('SELECT * FROM `user_follows` WHERE `user_login` = ? AND `user_follower_login` = ?',[req.params.login,req.user.login],(error,youFollow,fields)=>
+            {
+                if (error) return response.status(400,error,res);
+                row.youFollow = youFollow.length>0;
+                connection.query('SELECT * FROM `user_follows` WHERE `user_login` = ? AND `user_follower_login` = ?',[req.user.login,req.params.login],(error,followsYou,fields)=>
+                {
+                    if (error) return response.status(400,error,res);
+                    row.followsYou = followsYou.length>0;
+                    return response.status(200,row,res);
+                })
+            })
         }
-        else if (rows.length<1)
-        {
-            response.status(404,'User not found',res);
-        }
-        else
-        {
-            const row = rows[0];
-            response.status(200,row,res);
-        }
+        else return response.status(200,row,res);
     })
 }
 
@@ -90,15 +57,9 @@ exports.getLinks = (req,res) =>
 {
     connection.query('SELECT `url`,`description` FROM `user_links` WHERE `userLogin` = ?',[req.params.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            rows.forEach(row => { if (!row.description) row.description=row.url });
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        rows.forEach(row => { if (!row.description) row.description=row.url });
+        return response.status(200,rows,res);
     })
 }
 
@@ -107,14 +68,8 @@ exports.getAllSongs = (req,res) =>
     const sql = "SELECT `id`, ROW_NUMBER() OVER(PARTITION BY null) AS `pos` FROM(SELECT DISTINCT `id` FROM(SELECT `songID` as `id`,`time` FROM `song_likes` WHERE `userLogin` = ? UNION SELECT `songID` as `id`,`created_at` FROM `view_song_artists` WHERE `login` = ? ORDER by `time` DESC) as a) as b";
     connection.query(sql,[req.params.login,req.params.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -123,15 +78,8 @@ exports.getCreatedSongs = (req,res) =>
 
     connection.query('SELECT `songID` as `id`, ROW_NUMBER() OVER(PARTITION BY null ORDER BY `created_at` DESC) AS `pos` FROM `view_song_artists` WHERE `login` = ?',[req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -140,15 +88,8 @@ exports.getCreatedPopularSongs = (req,res) =>
 
     connection.query('SELECT `songID` as `id`, ROW_NUMBER() OVER(PARTITION BY null ORDER BY `likes_count` DESC) AS `pos` FROM `view_song_artists` WHERE `login` = ?',[req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -157,15 +98,8 @@ exports.getLikedSongs = (req,res) =>
 
     connection.query('SELECT `songID` as `id`, ROW_NUMBER() OVER(PARTITION BY null ORDER BY `song_likes`.`time` DESC) AS `pos` FROM `song_likes` WHERE `userLogin` = ?',[req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -174,15 +108,8 @@ exports.getAllPlaylists = (req,res) =>
     const sql="SELECT DISTINCT `id` FROM(SELECT `playlistID` as `id`,`time` FROM `playlist_likes` WHERE `userLogin` = ? UNION SELECT `playlistID` as `id`,`created_at` FROM `view_playlist_artists` WHERE `login` = ? ORDER by `time` DESC) as a";
     connection.query(sql,[req.params.login,req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -191,15 +118,8 @@ exports.getCreatedPlaylists = (req,res) =>
 
     connection.query('SELECT `playlistID` as `id` FROM `view_playlist_artists` WHERE `login` = ? ORDER BY `created_at` DESC',[req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -208,15 +128,8 @@ exports.getCreatedPopularPlaylists = (req,res) =>
 
     connection.query('SELECT `playlistID` as `id` FROM `view_playlist_artists` WHERE `login` = ? ORDER BY `likes_count` DESC',[req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -224,15 +137,8 @@ exports.getLikedPlaylists = (req,res) =>
 {
     connection.query('SELECT `playlistID` as `id` FROM `playlist_likes` WHERE `userLogin` = ? ORDER BY `playlist_likes`.`time` DESC',[req.params.login],(error,rows,fields)=>
     {
-
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -240,25 +146,14 @@ exports.getProfilePicture = (req,res) =>
 {
     connection.query("SELECT `profile_picture` FROM `users` WHERE `login` = ?",[req.params.login],(error,rows,fields)=>
     {
-        if (error)
+        if (error) return response.status(400,error,res);
+        if (rows.length<1) return response.status(404,'User not found',res);
+
+        const row = rows[0];
+        res.sendFile("images/user images/profile pictures/"+row.profile_picture,{root: '.'}, function (error)
         {
-            response.status(400,error,res);
-        }
-        else if (rows.length<1)
-        {
-            response.status(404,'User not found',res);
-        }
-        else
-        {
-            const row = rows[0];
-            res.sendFile("images/user images/profile pictures/"+row.profile_picture,{root: '.'}, function (error)
-            {
-                if (error)
-                {
-                  response.status(error.status,error,res);
-                }
-            });
-        }
+            if (error) return response.status(error.status,error,res);
+        });
     })
 }
 
@@ -266,25 +161,14 @@ exports.getBanner = (req,res) =>
 {
     connection.query("SELECT `banner` FROM `users` WHERE `login` = ?",[req.params.login],(error,rows,fields)=>
     {
-        if (error)
+        if (error) return response.status(400,error,res);
+        if (rows.length<1) return response.status(404,'User not found',res);
+
+        const row = rows[0];
+        res.sendFile("images/user images/banners/"+row.banner,{root: '.'}, function (error)
         {
-            response.status(400,error,res);
-        }
-        else if (rows.length<1)
-        {
-            response.status(404,'User not found',res);
-        }
-        else
-        {
-            const row = rows[0];
-            res.sendFile("images/user images/banners/"+row.banner,{root: '.'}, function (error)
-            {
-                if (error)
-                {
-                  response.status(error.status,error,res);
-                }
-            });
-        }
+            if (error) return response.status(error.status,error,res);
+        });
     })
 }
 
@@ -292,14 +176,8 @@ exports.getFollowers = (req,res) =>
 {
     connection.query('SELECT `user_follower_login` as `login` FROM `user_follows` WHERE `user_login` = ? ORDER BY `time` DESC',[req.params.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
     })
 }
 
@@ -308,13 +186,29 @@ exports.getFollowing = (req,res) =>
 
     connection.query('SELECT `user_login` as `login` FROM `user_follows` WHERE `user_follower_login` = ? ORDER BY `time` DESC',[req.params.login],(error,rows,fields)=>
     {
-        if (error)
-        {
-            response.status(400,error,res);
-        }
-        else
-        {
-            response.status(200,rows,res);
-        }
+        if (error) return response.status(400,error,res);
+        return response.status(200,rows,res);
+    })
+}
+
+exports.postFollow = (req,res) =>
+{
+    if (!req.user?.login) return response.status(401,"no auth",res);
+    const sql = 'INSERT INTO `user_follows`(`user_login`,`user_follower_login`,`time`) VALUES (?,?,?)';
+    connection.query(sql,[req.params.id,req.user.login,new Date().toISOString().slice(0, 19).replace('T', ' ')],(error,results)=>
+    {
+        if (error) return response.status(400,error,res);
+        return response.status(201,'Follow posted',res);
+    })
+}
+
+exports.deleteFollow = (req,res) =>
+{
+    if (!req.user?.login) return response.status(401,"no auth",res);
+    const sql = 'DELETE FROM `user_follows` WHERE `user_login`=? AND `user_follower_login`=?';
+    connection.query(sql,[req.params.id,req.user.login],(error,results)=>
+    {
+        if (error) return response.status(400,error,res);
+        return response.status(201,'Follow deleted',res);
     })
 }

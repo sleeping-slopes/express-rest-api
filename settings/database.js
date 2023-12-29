be3dd73066cfbd4f1f7e187d6eed957a2885dd4e
sleeping-slopes <weareinwin.rar@gmail.com>
2023-12-30 00:46:08 +0300

    replace connection with pool

--- settings/database.js
+++ settings/database.js
@@ -2,27 +2,31 @@ const mysql = require("mysql2");
 
 const config = require('../config');
 
-const connection=mysql.createConnection
+const pool=mysql.createPool
 (
     {
         host:config.DB_HOST,
         user:config.DB_USER,
         password:config.DB_PASSWORD,
         database:config.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 1,
+        idleTimeout: 10000,
+        queueLimit: 0
     }
 );
 
-connection.connect((error)=>
-{
-    if (error) return console.log("cannot connect to mysql database "+config.DB_NAME+" on "+config.DB_HOST);
-    return console.log("successfully connected to mysql database "+config.DB_NAME+" on "+config.DB_HOST);
-});
+// connection.connect((error)=>
+// {
+//     if (error) return console.log("cannot connect to mysql database "+config.DB_NAME+" on "+config.DB_HOST);
+//     return console.log("successfully connected to mysql database "+config.DB_NAME+" on "+config.DB_HOST);
+// });
 
 function queryPromise(query, params)
 {
     return new Promise((resolve, reject) =>
     {
-        connection.query(query, params, (err, result, fields) =>
+        pool.query(query, params, (err, result, fields) =>
         {
             if (err) reject(err);
             resolve(result);

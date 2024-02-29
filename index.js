554f2ea1bbae9b47d7330fe8cf7b8e2547442efd
sleeping-slopes <weareinwin.rar@gmail.com>
2024-02-29 16:08:58 +0300

    upd

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ routes(app);
 require('dotenv').config()
 
 console.log("server started");
+
+app.listen(process.env.PORT,(error)=>
+{
+    if (error) return console.log("server error");
+    console.log("server listen on port "+process.env.PORT);
+});

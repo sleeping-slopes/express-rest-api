b333204990972205c387b522cd1f9c4d458119f6
sleeping-slopes <weareinwin.rar@gmail.com>
2024-02-29 15:27:05 +0300

    upd

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const express = require('express');
-const cors = require("cors");
-const bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 5001
 
-const routes = require('./routes/routes');
-routes(app);
-
-require('dotenv').config()
-
-app.listen(process.env.PORT,(error)=>
-{
-    if (error) return console.log("server error");
-    console.log("server listen on port "+process.env.PORT);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const routes = require('./routes/routes');
+routes(app);
+
+require('dotenv').config()
+
+app.listen(process.env.PORT,(error)=>
+{
+    if (error) return console.log("server error");
+    console.log("server listen on port "+process.env.PORT);
+});
